43fc985874832dce9e86bf9b0123f74c89ad0258
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-05-21 13:43:30 +0100

    Adjust symbols for new Types

--- src/compiler/symbols/Function.java
+++ src/compiler/symbols/Function.java
@@ -2,11 +2,11 @@ package compiler.symbols;
 
 import java.util.List;
 
-public class FunctionType extends GenericType {
+public class Function extends GenericType {
     private Type returnType;
     private List<Type> parameterTypes;
 
-    public FunctionType(Type returnType, List<Type> parameterTypes) {
+    public Function(Type returnType, List<Type> parameterTypes) {
         setType(Type.FUNCTION);
         setReturnType(returnType);
         setParameterTypes(parameterTypes);
--- src/compiler/symbols/Symbol.java
+++ src/compiler/symbols/Symbol.java
@@ -1,16 +1,30 @@
 package compiler.symbols;
 
+import java.util.List;
+
 public class Symbol {
     private String identifier;
-    private Type type;
+    private GenericType type;
 
-    public Symbol(String identifier, Type type) {
+    public Symbol(String identifier, GenericType type) {
         this.identifier = identifier;
         this.type = type;
     }
 
     public Type getType() {
-        return type;
+        return type.getType();
+    }
+
+    public Type getReturnType() {
+        if (type.getType() == Type.FUNCTION)
+            return ((Function) type).getReturnType();
+        return null;
+    }
+
+    public List<Type> getParameterTypes() {
+        if (type.getType() == Type.FUNCTION)
+            return ((Function) type).getParameterTypes();
+        return null;
     }
 
     public String getIdentifier() {
