26650cf05325f262294e8517b78c8ea06caabc89
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-05-21 11:46:53 +0100

    Generate new Visitors

---
+++ src/compiler/codeGen.java
@@ -0,0 +1,261 @@
+@SuppressWarnings("CheckReturnValue")
+public class codeGen extends pdrawBaseVisitor<ST> {
+
+   @Override public ST visitProgram(pdrawParser.ProgramContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitStatement(pdrawParser.StatementContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitCoumpound(pdrawParser.CoumpoundContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitBlock(pdrawParser.BlockContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitDefine(pdrawParser.DefineContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitPenDefinition(pdrawParser.PenDefinitionContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitPropertyDefinition(pdrawParser.PropertyDefinitionContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitCanvasDefinition(pdrawParser.CanvasDefinitionContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitDeclaration(pdrawParser.DeclarationContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitWhile(pdrawParser.WhileContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitUntil(pdrawParser.UntilContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitFor(pdrawParser.ForContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitIf(pdrawParser.IfContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExecution(pdrawParser.ExecutionContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitPause(pdrawParser.PauseContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitStdout(pdrawParser.StdoutContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprToString(pdrawParser.ExprToStringContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprToBool(pdrawParser.ExprToBoolContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprToInt(pdrawParser.ExprToIntContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprMultDivMod(pdrawParser.ExprMultDivModContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprAddSub(pdrawParser.ExprAddSubContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprSetProperty(pdrawParser.ExprSetPropertyContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprInteger(pdrawParser.ExprIntegerContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprId(pdrawParser.ExprIdContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprString(pdrawParser.ExprStringContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprParent(pdrawParser.ExprParentContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprComp(pdrawParser.ExprCompContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprPoint(pdrawParser.ExprPointContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprStringConcat(pdrawParser.ExprStringConcatContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprColor(pdrawParser.ExprColorContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprToReal(pdrawParser.ExprToRealContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprPenOperator(pdrawParser.ExprPenOperatorContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprConvToRad(pdrawParser.ExprConvToRadContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprPenUnary(pdrawParser.ExprPenUnaryContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprUnary(pdrawParser.ExprUnaryContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprNew(pdrawParser.ExprNewContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprBoolOp(pdrawParser.ExprBoolOpContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprStdin(pdrawParser.ExprStdinContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprAssign(pdrawParser.ExprAssignContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprReal(pdrawParser.ExprRealContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitExprBool(pdrawParser.ExprBoolContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitPoint(pdrawParser.PointContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public ST visitBoolean(pdrawParser.BooleanContext ctx) {
+      ST res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+}
---
+++ src/compiler/semanticVerifier.java
@@ -0,0 +1,261 @@
+@SuppressWarnings("CheckReturnValue")
+public class semanticVerifier extends pdrawBaseVisitor<GenericType> {
+
+   @Override public GenericType visitProgram(pdrawParser.ProgramContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitStatement(pdrawParser.StatementContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitCoumpound(pdrawParser.CoumpoundContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitBlock(pdrawParser.BlockContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitDefine(pdrawParser.DefineContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitPenDefinition(pdrawParser.PenDefinitionContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitPropertyDefinition(pdrawParser.PropertyDefinitionContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitCanvasDefinition(pdrawParser.CanvasDefinitionContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitDeclaration(pdrawParser.DeclarationContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitWhile(pdrawParser.WhileContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitUntil(pdrawParser.UntilContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitFor(pdrawParser.ForContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitIf(pdrawParser.IfContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExecution(pdrawParser.ExecutionContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitPause(pdrawParser.PauseContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitStdout(pdrawParser.StdoutContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprToString(pdrawParser.ExprToStringContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprToBool(pdrawParser.ExprToBoolContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprToInt(pdrawParser.ExprToIntContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprMultDivMod(pdrawParser.ExprMultDivModContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprAddSub(pdrawParser.ExprAddSubContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprSetProperty(pdrawParser.ExprSetPropertyContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprInteger(pdrawParser.ExprIntegerContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprId(pdrawParser.ExprIdContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprString(pdrawParser.ExprStringContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprParent(pdrawParser.ExprParentContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprComp(pdrawParser.ExprCompContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprPoint(pdrawParser.ExprPointContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprStringConcat(pdrawParser.ExprStringConcatContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprColor(pdrawParser.ExprColorContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprToReal(pdrawParser.ExprToRealContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprPenOperator(pdrawParser.ExprPenOperatorContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprConvToRad(pdrawParser.ExprConvToRadContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprPenUnary(pdrawParser.ExprPenUnaryContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprUnary(pdrawParser.ExprUnaryContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprNew(pdrawParser.ExprNewContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprBoolOp(pdrawParser.ExprBoolOpContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprStdin(pdrawParser.ExprStdinContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprAssign(pdrawParser.ExprAssignContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprReal(pdrawParser.ExprRealContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitExprBool(pdrawParser.ExprBoolContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitPoint(pdrawParser.PointContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public GenericType visitBoolean(pdrawParser.BooleanContext ctx) {
+      GenericType res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+}
