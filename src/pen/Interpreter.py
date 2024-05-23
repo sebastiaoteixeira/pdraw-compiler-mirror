b9369df652d0bf3e17bdf306f21149afa747c208
martimsantos <santos.martim@ua.pt>
2024-05-23 14:54:34 +0100

    interpreter novo e gramática secundária fixed

---
+++ src/pen/Interpreter.py
@@ -0,0 +1,116 @@
+from antlr4 import *
+from penParser import penParser
+from penVisitor import penVisitor
+
+class Interpreter(penVisitor):
+   def visitProgram(self, ctx:penParser.ProgramContext):
+      return self.visitChildren(ctx)
+
+   def visitDeclaration_element(self, ctx:penParser.Declaration_elementContext):
+      return self.visitChildren(ctx)
+
+   def visitDeclaration(self, ctx:penParser.DeclarationContext):
+      return self.visitChildren(ctx)
+
+   def visitStatement(self, ctx:penParser.StatementContext):
+      return self.visitChildren(ctx)
+
+   def visitWhile(self, ctx:penParser.WhileContext):
+      return self.visitChildren(ctx)
+
+   def visitUntil(self, ctx:penParser.UntilContext):
+      return self.visitChildren(ctx)
+
+   def visitFor(self, ctx:penParser.ForContext):
+      return self.visitChildren(ctx)
+
+   def visitIf(self, ctx:penParser.IfContext):
+      return self.visitChildren(ctx)
+
+   def visitPause(self, ctx:penParser.PauseContext):
+      return self.visitChildren(ctx)
+
+   def visitStdout(self, ctx:penParser.StdoutContext):
+      return self.visitChildren(ctx)
+
+   def visitExprToString(self, ctx:penParser.ExprToStringContext):
+      return self.visitChildren(ctx)
+
+   def visitExprToBool(self, ctx:penParser.ExprToBoolContext):
+      return self.visitChildren(ctx)
+
+   def visitExprToInt(self, ctx:penParser.ExprToIntContext):
+      return self.visitChildren(ctx)
+
+   def visitExprMultDivMod(self, ctx:penParser.ExprMultDivModContext):
+      return self.visitChildren(ctx)
+
+   def visitExprSetProperty(self, ctx:penParser.ExprSetPropertyContext):
+      return self.visitChildren(ctx)
+
+   def visitExprAddSub(self, ctx:penParser.ExprAddSubContext):
+      return self.visitChildren(ctx)
+
+   def visitExprInteger(self, ctx:penParser.ExprIntegerContext):
+      return self.visitChildren(ctx)
+
+   def visitExprId(self, ctx:penParser.ExprIdContext):
+      return self.visitChildren(ctx)
+
+   def visitExprPi(self, ctx:penParser.ExprPiContext):
+      return self.visitChildren(ctx)
+
+   def visitExprString(self, ctx:penParser.ExprStringContext):
+      return self.visitChildren(ctx)
+
+   def visitExprParent(self, ctx:penParser.ExprParentContext):
+      return self.visitChildren(ctx)
+
+   def visitExprComp(self, ctx:penParser.ExprCompContext):
+      return self.visitChildren(ctx)
+
+   def visitExprPoint(self, ctx:penParser.ExprPointContext):
+      return self.visitChildren(ctx)
+
+   def visitExprStringConcat(self, ctx:penParser.ExprStringConcatContext):
+      return self.visitChildren(ctx)
+
+   def visitExprColor(self, ctx:penParser.ExprColorContext):
+      return self.visitChildren(ctx)
+
+   def visitExprToReal(self, ctx:penParser.ExprToRealContext):
+      return self.visitChildren(ctx)
+
+   def visitExprPenOperator(self, ctx:penParser.ExprPenOperatorContext):
+      return self.visitChildren(ctx)
+
+   def visitExprConvToRad(self, ctx:penParser.ExprConvToRadContext):
+      return self.visitChildren(ctx)
+
+   def visitExprPenUnary(self, ctx:penParser.ExprPenUnaryContext):
+      return self.visitChildren(ctx)
+
+   def visitExprUnary(self, ctx:penParser.ExprUnaryContext):
+      return self.visitChildren(ctx)
+
+   def visitExprBoolOp(self, ctx:penParser.ExprBoolOpContext):
+      return self.visitChildren(ctx)
+
+   def visitExprStdin(self, ctx:penParser.ExprStdinContext):
+      return self.visitChildren(ctx)
+
+   def visitExprAssign(self, ctx:penParser.ExprAssignContext):
+      return self.visitChildren(ctx)
+
+   def visitExprReal(self, ctx:penParser.ExprRealContext):
+      return self.visitChildren(ctx)
+
+   def visitExprBool(self, ctx:penParser.ExprBoolContext):
+      return self.visitChildren(ctx)
+
+   def visitAssign(self, ctx:penParser.AssignContext):
+      return self.visitChildren(ctx)
+
+   def visitPoint(self, ctx:penParser.PointContext):
+      return self.visitChildren(ctx)
+
--- src/pen/pen.g4
+++ src/pen/pen.g4
@@ -1,33 +1,100 @@
 grammar pen;
 
-program: command* EOF;
+program: statement* EOF;
 
-command: setOrientation
-        | setPressure
-        | setThickness
-        | setColor
-        | setPosition
-        | penControl
-        | penMove
-        | rotate
-        ;
+declaration_element: (ID | assign);
 
-setOrientation: 'orientation' expr ('º')? ';';
-setPressure: 'pressure' expr ';';
-setThickness: 'thickness' expr ';';
-setColor: 'color'  expr ';';
-setPosition: 'position' '(' expr ',' expr ')' ';';
-penControl: ('up' | 'down') ';';
-penMove: ('forward' | 'backward') expr ';';
-rotate: ('left' | 'right') expr ('º')? ';';
+declaration: type=Type declaration_element (',' declaration_element)*;
 
-expr: Integer 
-    | Real
-    | '(' expr ')'
+statement: (pause | stdout | expression | for | if | while | until | declaration) ';';
+
+while: 'while' '(' expression ')' statement;
+
+until: 'until' '(' expression ')' statement;
+
+for: 'for' '(' declaration ';' expression ';' expression ')' statement; 
+
+if: 'if' '(' expression ')' statement ('else'  statement )?;
+
+pause: 'pause' expression;
+
+stdout: expression '->' 'stdout';
+
+expression:
+    op=('+'|'-') expression #ExprUnary
+    | expression op='º' #ExprConvToRad
+    | expression op=('*'|'/'|'//'|'\\\\') expression #ExprMultDivMod
+    | expression op=('+'|'-') expression #ExprAddSub
+
+    | expression expression #ExprStringConcat
+
+    | 'int' '(' expression ')' #ExprToInt
+    | 'real' '(' expression ')' #ExprToReal
+    | 'string' '(' expression ')' #ExprToString
+    | 'bool' '(' expression ')' #ExprToBool
+
+    | op=('down'|'up') #ExprPenUnary
+    | op=('forward'|'left'|'right'|'backward') expression #ExprPenOperator
+    | Property expression #ExprSetProperty
+
+    | 'stdin' expression #ExprStdin
+
+    // Comparison
+    | expression op=('=='|'!='|'<'|'<='|'>'|'>=') expression #ExprComp
+
+    // Boolean expressions
+    | expression op=('and'|'or') expression #ExprBoolOp
+
+    // Assign
+    | assign #ExprAssign
+
+    // Litterals
+    | Integer #ExprInteger
+    | Real #ExprReal
+    | String #ExprString
+    | Color #ExprColor
+    | point #ExprPoint
+    | Boolean #ExprBool
+    | PI #ExprPi
+
+    // Identifier
+    | ID #ExprId
+
+    // Parentized expression
+    | '(' expression ')' #ExprParent
     ;
+
+
+// Assign 
+assign: ID op='=' expression;
+
+// Types
+Type: ('pen'|'real'|'int'|'string'|'point'|'bool');
+point: '(' x=expression ',' y=expression ')';
+
+Property: ('color'|'pressure'|'thickness'|'orientation'|'position');
+
+String: '"' (EscapeSequence | ~['"\\])* '"';
+fragment EscapeSequence: '\\' . ;
 
 Integer: [0-9]+;
 Real: [0-9]+ '.' [0-9]+;
 
+Boolean: 'true' | 'false';
+
+Color: ('white'|'black'|'green'|'red'|'blue'|'yellow')
+| '#' HEX HEX HEX HEX HEX HEX;
+
+PI: 'PI';
+
+fragment HEX: [0-9a-fA-F];
+
+// Identifiers
+ID: [a-zA-Z_][a-zA-Z_0-9]*;
+
+// Comments '%'
 COMMENT: '%' ~[\n]* -> skip;
+
+
+// Whitespace
 WS: [ \t\r\n]+ -> skip;
